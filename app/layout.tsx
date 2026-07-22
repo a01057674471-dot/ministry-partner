3a612a91a1872259f5adbc10cb0abccde2b0ac62
a01057674471-dot <a01057674471@gmail.com>
2026-07-22 20:48:34 +0900

    Load mobile-first V3 styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,12 +10,13 @@ import "./account-cloud.css";
 import "./product-upgrades.css";
 import "./v2.css";
 import "./v2-global.css";
+import "./mobile-v3.css";
 import CloudSyncBridge from "./components/CloudSyncBridge";
 import GlobalChrome from "./components/GlobalChrome";
 
 export const metadata: Metadata = {
   title: "목회파트너",
-  description: "목회자의 설교, 기도, 문서와 이미지 콘텐츠 준비를 돕는 사역 파트너",
+  description: "말씀과 성도를 섬기는 목회자의 사역 준비를 돕는 동역자",
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
